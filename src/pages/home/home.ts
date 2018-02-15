86226c9d0105f11bb7feb4138cb088178fc5d0c7
Dillon Ward <Dillonward13@gmail.com>
2018-02-15 17:43:59 +0000

    templating buttons to starting screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  buttons: any;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  ngOnInit(){
+
+    this.buttons = [
+      {
+        id: "start",
+        text: "Start"
+      },
+      {
+        id: "exit",
+        text: "Exit"
+      }
+    ]
+   
+    
+  }
 }
